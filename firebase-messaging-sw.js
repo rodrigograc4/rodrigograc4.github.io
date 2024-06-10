6eead4cdcbd7cb74e38661319d9767abc763f0f6
Rodrigo Graça <117362800+rodrigograc4@users.noreply.github.com>
2024-06-10 13:01:54 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,30 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDJqIRd11jCSzx64_8dpWPVpIdNFzl07RE",
-  authDomain: "apexhub-f1.firebaseapp.com",
-  projectId: "apexhub-f1",
-  storageBucket: "apexhub-f1.appspot.com",
-  messagingSenderId: "931355642260",
-  appId: "1:931355642260:web:a439e04c49bf8d5d1fa781",
-  measurementId: "G-0KWSCPC724"
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyDJqIRd11jCSzx64_8dpWPVpIdNFzl07RE",
+//   authDomain: "apexhub-f1.firebaseapp.com",
+//   projectId: "apexhub-f1",
+//   storageBucket: "apexhub-f1.appspot.com",
+//   messagingSenderId: "931355642260",
+//   appId: "1:931355642260:web:a439e04c49bf8d5d1fa781",
+//   measurementId: "G-0KWSCPC724"
+// });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
-console.log('[firebase-messaging-sw.js] Inicializando firebase-messaging-sw.js');
+// console.log('[firebase-messaging-sw.js] Inicializando firebase-messaging-sw.js');
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Recebida mensagem em segundo plano', payload);
+// messaging.onBackgroundMessage((payload) => {
+//   console.log('[firebase-messaging-sw.js] Recebida mensagem em segundo plano', payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-      body: payload.notification.body,
-      icon: 'https://rodrigograc4.github.io/ApexHub-F1/Images/ApexIcon_v2-01.png'
-  };
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//       body: payload.notification.body,
+//       icon: 'https://rodrigograc4.github.io/ApexHub-F1/Images/ApexIcon_v2-01.png'
+//   };
 
-  console.log('[firebase-messaging-sw.js] Exibindo notificação', notificationTitle);
+//   console.log('[firebase-messaging-sw.js] Exibindo notificação', notificationTitle);
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
